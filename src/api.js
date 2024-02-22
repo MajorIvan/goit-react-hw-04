8d52a05d86d77112bd1800e1259ccbd325842fd3
Ivan Moshak <ivanmoshak11@gmail.com>
2024-02-22 16:46:01 +0200

    update

--- src/api.js
+++ src/api.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
 const fetchImages = async (query, page) => {
-    const accessKey = "piE9EcbbHjRxyOxFyLQbzpSYxKOax2U8dY2kGWHPIuw";
-    const response = await axios.get(
-        `https://api.unsplash.com/search/photos/?client_id=${accessKey}`, {
-            params: { query, page, per_page: 20 }});
+  const accessKey = "piE9EcbbHjRxyOxFyLQbzpSYxKOax2U8dY2kGWHPIuw";
+  const response = await axios.get(
+    `https://api.unsplash.com/search/photos/?client_id=${accessKey}`,
+    {
+      params: { query, page, per_page: 20, orientation: "landscape" },
+    }
+  );
 
-    return response.data;
-}
+  return response.data;
+};
 
 export default fetchImages;
